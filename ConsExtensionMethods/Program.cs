bf58ab2bade1927ebea844d3f787c877dd48a93c
William Schokkelé <william.schokkele@howest.be>
2018-02-23 10:18:24 +0100

    Add project files.

---
+++ ConsExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
